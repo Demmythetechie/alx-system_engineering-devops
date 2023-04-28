9669ceb0f76c6368ae0a654767a59691679538e7
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-04-28 18:45:30 +0000

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 # Added a private key file and PasswordAuthentication
 file_line {'identifiying private key file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '   Identify ~/.ssh/school',
-  match  => '^   Identify',
+  ensure   => 'present',
+  path     => '/etc/ssh/ssh_config',
+  line     => '   Identify ~/.ssh/school',
+  match    => '^#   IdentityFile',
+  replace  => 'false',
 }
 
 file_line {'PasswordAuthentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '   PasswordAuthentication no',
-  match  => '^   PasswordAuthentication',
+  match  => '^#   PasswordAuthentication',
 }
