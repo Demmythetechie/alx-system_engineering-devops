b786a07ae80a0213b5591b4776148d3a2a15f09d
demmythetechie@gmail.com <demmythetechie@gmail.com>
2023-04-28 18:17:57 +0000

    4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,4 +9,5 @@ file_line {'PasswordAuthentication':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '   PasswordAuthentication no',
+  match  => '^   PasswordAuthentication',
 }
