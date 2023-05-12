7e25f0b19919cfe7fb319e516e06a1930fd2725a
Demmythetechie <109427907+Demmythetechie@users.noreply.github.com>
2023-05-12 07:46:39 +0100

    Delete practice directory

--- practice/task.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def array_diff(a, b):
-    s1 = {a[i] for i in range(len(a))}
-    s2 = {b[i] for i in range(len(b))}
-
-    print(s1.symmetric.difference(s2))
-
-array_diff([2, 3, 4, 6], [1, 8, 6])
