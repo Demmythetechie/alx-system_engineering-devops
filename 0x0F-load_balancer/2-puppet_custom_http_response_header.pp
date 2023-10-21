e2b0e3445a71b9d207d9f94279e120d6de85416d
Demmythetechie <demmythetechie@gmail.com>
2023-10-21 14:22:39 +0100

    2. Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,9 @@
 # This puppet script adds a custom HTTP header with puppet
 
-file_line { 'insert line at number 52':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  line   => 'add_header X-Served-By $hostname',
-  match  => '',
-  after  => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
 }
